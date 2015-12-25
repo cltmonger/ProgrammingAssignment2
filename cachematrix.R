8be737ee2da0693a900f40e522bf41ebf15f4029
cltmonger <cltmonger@gmail.com>
2015-12-25 14:55:49 +0100

    first

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is minimalist approach, as far as I understood the assignment
+## it is about proper comments proving undestanding of subject, 
+## as the code provided is sufficient to get a solution with only minor changes
 
-## Write a short comment describing this function
+
+#Reads in a matrix and returns a list of functions:
+#returning given matrix, changing saved matrix, setting inverse matrix and getting inverse matrix
+#as values are set in function's environment, they can be easily approached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL 
+        set <- function(y) {
+                x <<- y ## when used sets value of x for parent:whole makeCacheMatrix function      
+                inv <<- NULL #when new matrix is set, resets inverse matrix to NULL
+        }
+        get <- function() x #returns x which is matrix set for this functions instance
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## works on lists of functions like makeCacheMatrix. 
+#Reads saved inverse matrix, in case it is not empty returns message and the matrix
+#in case there isn't any, reads matrix, finds inverse matrix solution and sets it in makeCaheMatrix type object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        
+                inv <- x$getinverse()
+                if(!is.null(inv)) {
+                        message("getting cached inverse matrix")
+                        return(inv)
+                }
+                data <- x$get()
+                inv <- solve(data, ...)
+                x$setinverse(inv)
+                inv        ## Return a matrix that is the inverse of 'x'
+ }
